9e9f3108c204e29f345117a0c0995417b856f47b
Seth-Harlaar <92818054+Seth-Harlaar@users.noreply.github.com>
2024-01-06 09:10:22 -0500

    more gh-pages

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ root.render(
   <React.StrictMode>
     <ScreenProvider>
       <ModalProvider>
-        <BrowserRouter>
+        <BrowserRouter basename='/website'>
           <Main/>
         </BrowserRouter>
       </ModalProvider>
